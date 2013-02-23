cea63d7c6339cbbb089b6a30dd85017ca2becf14
Shibly <shibly.phy@gmail.com>
2013-02-23 15:51:01 +0600

    initial commit

---
+++ application/controllers/admin/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Shibly
+ * Date: 2/23/13
+ * Time: 3:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
---
+++ application/libraries/Frontend_Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Shibly
+ * Date: 2/23/13
+ * Time: 3:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Frontend_Controller extends MY_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+}
